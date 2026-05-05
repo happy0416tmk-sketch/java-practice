41cbe796558717b3947937d697898da94398c34d
happy0416tmk-sketch <sshen4088@gmail.com>
2026-05-06 01:55:10 +0900

    Update text.java
    確認して

--- テキスト/text.java
+++ テキスト/text.java
@@ -5,16 +5,23 @@ public class text extends JFrame {
     public text () {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setTitle("テキスト");
-        setSize(400, 200);
-        setVisible(true);
+        setSize(400, 400);
         setLayout(new FlowLayout());
 
         JLabel label = new JLabel("ここにテキストが表示されます");
+        JTextField textField = new JTextField(15);
+        JButton button = new JButton("表示する");
+
+        button.addActionListener(e -> {
+            label.setText(textField.getText());
+        });
 
         add(label);
-        
+        add(textField);
+        add(button);
 
-        
+        setVisible(true);
+       
     }
 
     public static void main(String[] args) {
